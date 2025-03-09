95ece2a46a757d16d238269dec393c821ee6107d
swiftmg0d <zhivche.gligorov@gmail.com>
2025-03-09 20:04:24 +0100

    changed navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import Image from "next/image";
+// Import the Background component and NavbarDemo component
+import Background from "@/components/background";
+import { FloatingNavDemo } from "@/components/navbar";
+import { Oxanium } from "next/font/google";
+
+const oxanium = Oxanium({
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <div>zive ne jadi g..</div>
+    <>
+      <Background />
+      <FloatingNavDemo />
+      <div className="flex items-center justify-center w-full h-screen text-white relative z-10">
+        <div className={`text-center w-[840px] font-bold p-6 `}>
+          <p
+            className={`drop-shadow-md text-[25px] text-custom-white ${oxanium.className}`}
+          >
+            Track and analyze your sports performance in real-time. Gain
+            insights, visualize progress, and connect with fellow athletes.
+            Elevate your game with ActiTrack!
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ src/components/background.tsx
@@ -0,0 +1,28 @@
+"use client"; 
+import React, { useState, useEffect } from "react";
+
+// Background Component
+export default function Background() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; 
+  }
+
+  return (
+    <div className="absolute top-0 left-0 w-full h-full z-0">
+      <video
+        autoPlay
+        loop
+        muted
+        className="w-full h-full object-cover fixed"
+      >
+        <source src="home_video.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
